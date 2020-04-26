c1edfbdb528620ad35d58a7dee20769add3cc05c
vinaytshetty <vinaytshetty@gmail.com>
2020-04-26 13:29:39 +0530

    Navigation Drawer First Stage

---
+++ app/src/main/java/com/navheader/MainActivity.java
@@ -0,0 +1,12 @@
+package com.navheader;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_drawer_layout);
+    }
+}
