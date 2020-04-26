cbbc67df461bc71ab7b86ab8648ec7a3c13db25a
vinaytshetty <vinaytshetty@gmail.com>
2020-04-26 20:07:12 +0530

    1.Replaced default ActionBar with custom toolbar 2.Added Hamburger Icon along with default Animation 3.Made default status bar transperent and compatible for both API 21 and below

--- app/src/main/java/com/navheader/MainActivity.java
+++ app/src/main/java/com/navheader/MainActivity.java
@@ -1,12 +1,56 @@
 package com.navheader;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
+import android.view.Gravity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mdrawerLayout;
+    Toolbar mtoolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_drawer_layout);
+        initViews();
+        usetoolbar();
+        addHumberDesingnAndSync();
+    }
+
+
+    private void initViews() {
+        mtoolbar = (Toolbar) findViewById(R.id.toolbar_mainActivity);
+        mdrawerLayout = findViewById(R.id.drawerlayout);
+    }
+
+    private void usetoolbar() {
+        setSupportActionBar(mtoolbar);
+    }
+
+    private void addHumberDesingnAndSync(){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                mdrawerLayout,
+                mtoolbar,
+                R.string.nav_open,
+                R.string.nav_close);
+        mdrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if(mdrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mdrawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 }
